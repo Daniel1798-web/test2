45d2ba731ffb7f0d5ba11364f0996fa3a8a3d5cb
Daniel Oto Diaz <danieloto212@gmail.com>
2022-06-14 17:27:20 -0300

    add characters in locations

--- src/app/website/pages/location/location.component.ts
+++ src/app/website/pages/location/location.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
 import { ActivatedRoute } from '@angular/router';
+import { Character } from 'src/app/models/character.model';
 
 @Component({
   selector: 'app-location',
@@ -13,18 +14,44 @@ export class LocationComponent implements OnInit {
     private route: ActivatedRoute
     ) { }
 
+    solo = []
+
   idLocation:string="";
   location
   cosa = "Alive";
   cosa2 = "Dead"
   cosa3 = "unknown"
+  idCharacter:string =""
+  character:any=[]
+  character2:any=[]
 
   ngOnInit(): void {
     this.idLocation = this.route.snapshot.params["id"]
     this.apiService.getOneLocation(this.idLocation)
     .subscribe(data=>{
       this.location = data
+      this.solo = this.location.residents
+
+     // console.log(this.location.residents)
     })
+
+    this.apiService.getAllCharacters()
+    .subscribe(data=>{
+      this.character = data
+      
+    })
+    this.fors()
+
   }
 
-}
+
+  fors(){
+    for(let i:any = 0; i < this.solo.length; i++){
+      i = this.solo
+      console.log(i)
+      }
+    }
+
+  }
+
+
